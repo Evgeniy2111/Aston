2d0c39319b2fe71b209ad942d2c0f93e30ae2043
Eвгений2111 <evgen21111993@gmail.com>
2025-03-31 17:06:52 +0300

    Leasson_1 v1.2

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,15 @@ public class Main {
         checkSumSing();
         printColor();
         compareNumbers();
-        System.out.println(sumOfNumbers(12,25));
-        System.out.println(numberValue(-10));
-        System.out.println(duplicateLine("WARNING",10 ));
-        System.out.println(leapYear(1600));
+        sumOfNumbers(10,8);
+        numberValue(-5);
+        duplicateLine("Warning", 3);
+        leapYear(1600);
         replacingNumbers();
         arrayOfNumber();
         arrayNumbers();
         cubArray();
-        arrayString(5,10);
+        arrayString(5, 10);
     }
 
     public static void printThreeWords() {
@@ -34,10 +34,10 @@ public class Main {
     }
 
     public static void printColor() {
-        int value = 101;
+        int value = 53;
         if (value <= 0) {
             System.out.println("Красный");
-        }  else if (value > 0 && value <= 100) {
+        } else if (value <= 100) {
             System.out.println("Желтый");
         } else {
             System.out.println("Зеленый");
@@ -45,10 +45,10 @@ public class Main {
     }
 
     public static void compareNumbers() {
-        int a = 10;
-        int b = 5;
+        int a = 3;
+        int b = 15;
         boolean c = a >= b;
-        if (c == true ) {
+        if (c) {
             System.out.println("a >= b");
         } else {
             System.out.println("a < b");
@@ -57,73 +57,58 @@ public class Main {
 
     public static boolean sumOfNumbers(int a, int b) {
         int sum = a + b;
-       if (sum >= 10 && sum <= 20)
-           return true;
-       else
-           return false;
+        return sum >= 10 && sum <= 20;
     }
 
     public static boolean numberValue(int a) {
-        if (a >= 0)
-            return false;
-        else
-            return true;
+        return a >= 0;
     }
 
-    public static String duplicateLine(String String, int a) {
+    public static void duplicateLine(String String, int a) {
         for (int i = 1; i < a; i++) {
             System.out.println(String);
         }
-        return String;
-        /* через while:
-        int i = 1;
-        while(i < a)
-        System.out.println(String);
-        i++;
-         */
     }
 
-    public static boolean leapYear(int a) {
-        for (;;a++) {
-            if (a % 4 == 0 && a % 100 != 0 || a % 400 ==0)
-                return true;
-        }
+    public static boolean leapYear(int a) {//цикл я включил, тк затупил, решил, что нужно перебор дат делать будет за какой то период
+        return a % 4 == 0 && a % 100 != 0 || a % 400 == 0;
     }
 
     public static void replacingNumbers() {
-        int [] numbers = {1,0,0,1,0,1,0,0,1};
+        int[] numbers = {1, 0, 0, 1, 0, 1, 0, 0, 1};
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers [i] == 0) {
+            if (numbers[i] == 0) {
                 numbers[i] = 1;
-            }else {
+            } else {
                 numbers[i] = 0;
             }
-            System.out.println(Arrays.toString(numbers));
         }
+        System.out.println("Измененный массив: " + Arrays.toString(numbers));
     }
 
     public static void arrayOfNumber() {
         int[] arraySize = new int[100];
-        for (int i = 0; i <= arraySize.length; i++) {
-            System.out.println(i);
+        for (int i = 0; i < arraySize.length; i++) {
+            arraySize[i] = i; //почему в данном случае не нужно прибавлять 1 или записывать ++,+=, из-за итерации?
         }
+        System.out.println(Arrays.toString(arraySize));
     }
 
     public static void arrayNumbers() {
-        int [] myArray = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        int[] myArray = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for (int i = 0; i < myArray.length; i++) {
             if (myArray[i] < 6) {
                 myArray[i] = myArray[i] * 2;
             }
-            System.out.println(Arrays.toString(myArray));
         }
+        System.out.println(Arrays.toString(myArray));
     }
 
     public static void cubArray() {
-        int [][] cub = new int[10][10];
+        int[][] cub = new int[10][10];
         for (int i = 0; i < cub.length; i++) {
             for (int j = 0; j < cub[0].length; j++) {
-                if (cub[i] == cub[j] ) {
+                if (cub[i] == cub[j]) {
                     cub[i][j] = 1;
                 } else {
                     cub[i][j] = 0;
@@ -135,11 +120,12 @@ public class Main {
         }
     }
 
-    public static void arrayString(int len, int initialValue) {
-       int [] stringArray = new int[len];
-       for (int i = 0; i < len; i++) {
-           stringArray[i] = initialValue;
-       }
-       System.out.println(Arrays.toString(stringArray));
+    public static int[] arrayString(int len, int initialValue) {
+        int[] stringArray = new int[len];
+        for (int i = 0; i < len; i++) {
+            stringArray[i] = initialValue;
+        }
+        System.out.println(Arrays.toString(stringArray));
+        return stringArray;
     }
 }
