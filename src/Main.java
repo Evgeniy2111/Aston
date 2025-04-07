05945ccdc4a8d476eb8f7f97fd0ba88df279b850
Evgeniy2111 <evgen21111993@gmail.com>
2025-04-07 23:31:13 +0300

    Leasson_12

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+
+import static java.lang.Long.sum;
+
+public class Main {
+    public static void main(String[] args) throws MyArraySizeException, MyArrayDataException {
+
+        String[][] myArrayCorrect = new String[][]{
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+
+
+        String[][] myArray1 = new String[][]{
+                {"1", "10", "11", "12"},
+                {"2", "20", "21", "22"},
+                {"3", "30", "31", "32"}
+        };
+
+        String[][] myArray3 = new String[][]{
+                {"F", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "M", "12"},
+                {"13", "14", "15", "16"}
+        };
+
+
+        try {
+            MyArray.myArraySize(myArrayCorrect);
+            System.out.println("Все данные верны." + "\nСумма числовыхх значений равна: " + MyArray.count );
+        } catch (MyArraySizeException e) {
+            System.out.println(e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println();
+        try {
+            MyArray.myArraySize(myArray1);
+            System.out.println("Все данные верны." + "\nСумма числовыхх значений равна: " + MyArray.count );
+        } catch (MyArraySizeException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println();
+        try {
+            MyArray.myArraySize(myArray3);
+            System.out.println("Все данные верны." + "\nСумма числовыхх значений равна: " +  MyArray.count);
+        } catch (MyArrayDataException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+}
---
+++ src/MyArray.java
@@ -0,0 +1,29 @@
+public class MyArray {
+
+    static int count = 0;
+
+    public static int myArraySize(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        if (array.length != 4) {
+            throw new MyArraySizeException("Размер массива не соответствует требованиям!");
+        }
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (array[i].length != 4) {
+                throw new MyArraySizeException("Размер массива не соответствует требованиям");
+            }
+
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    count += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Не верное значение " + "'" + array[i][j] + "'" +
+                            " в ячейке" + "[" + i + "]" + "[" + j + "]");
+                }
+            }
+        }
+        return count;
+    }
+
+
+}
